b7346ef9cd3c02abc1cfec48584309da993e0c3d
hamzasaadani <95533564+hamzasaadani@users.noreply.github.com>
2022-02-14 22:23:39 +0100

    Add files via upload

---
+++ final project/DOM.JS
@@ -0,0 +1,51 @@
+var buttonsPlus = Array.from(document.getElementsByClassName("add"));
+
+for (let btnPlus of buttonsPlus) {
+  btnPlus.addEventListener("click", function () {
+    btnPlus.nextElementSibling.innerHTML++;
+    sum();
+  });
+}
+
+var buttonsMinus = Array.from(document.getElementsByClassName("minus"));
+
+for (let i = 0; i < buttonsPlus.length; i++) {
+  buttonsMinus[i].addEventListener("click", function () {
+    if (buttonsMinus[i].previousElementSibling.innerText > 0)
+      buttonsMinus[i].previousElementSibling.innerText--;
+    sum();
+  });
+}
+
+function sum() {
+  let prices = document.getElementsByClassName("price");
+  let qtes = document.getElementsByClassName("qte");
+  let s = 0;
+
+  console.log(prices[0].innerHTML);
+  console.log(qtes[0].innerHTML);
+  for (let i = 0; i < prices.length; i++) {
+    s += prices[i].innerHTML * qtes[i].innerHTML;
+  }
+
+  return (document.getElementById("totalp").innerText = "$ " + s);
+}
+
+let trash = document.getElementsByClassName("fa-trash-alt");
+
+for (let btnTrash of trash) {
+  btnTrash.addEventListener("click", function () {
+    btnTrash.parentNode.remove();
+    sum();
+  });
+}
+
+let hearts = document.getElementsByClassName("fa-heart");
+
+for (let heart of hearts) {
+  heart.addEventListener("click", function () {
+
+    heart.classList.toggle("red");
+
+  });
+}
